af981d3cbbd1748c1dcca2235789f9e8e84a5d7b
EthicalHackingAI <ethicalhackingai.dev@gmail.com>
2026-03-30 07:34:20 +0000

    rebuild ssg with 27 blog posts

--- artifacts/ethical-hacking/src/App.tsx
+++ artifacts/ethical-hacking/src/App.tsx
@@ -6,7 +6,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { HelmetProvider } from "react-helmet-async";
 import Home from "@/pages/home";
 import Tools from "@/pages/tools";
-import ToolDetail from "@/pages/tool-detail";
 import BestToolsPage from "@/pages/BestToolsPage";
 import BlogIndex from "@/pages/BlogIndex";
 import BlogPost from "@/pages/BlogPost";
@@ -39,7 +38,6 @@ function Router() {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/tools" component={Tools} />
-        <Route path="/tools/:slug" component={ToolDetail} />
         <Route path="/best/:slug" component={BestToolsPage} />
         <Route path="/blog" component={BlogIndex} />
         <Route path="/blog/:slug" component={BlogPost} />
